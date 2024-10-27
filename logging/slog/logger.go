b4fe74b5166d6a08388bcc570650dc53665a7e94
zstone12 <522089185@qq.com>
2024-10-27 17:31:47 +0800

    feat: slog init func

--- logging/slog/logger.go
+++ logging/slog/logger.go
@@ -1,1 +1,32 @@
 package slog
+
+import (
+	"fmt"
+	"log/slog"
+)
+
+type Writer struct {
+	log    *slog.Logger
+	config *config
+}
+
+func NewWriter(opts ...Option) *Writer {
+	cfg := defaultConfig()
+
+	// apply options
+	for _, opt := range opts {
+		opt.apply(cfg)
+	}
+
+	logger := slog.New(NewTraceHandler(cfg.coreConfig.writer, cfg.coreConfig.opt, cfg.traceConfig))
+
+	return &Writer{log: logger, config: cfg}
+}
+
+func (w *Writer) Logger() *slog.Logger {
+	return w.log
+}
+
+func (w *Writer) Printf(format string, v ...interface{}) {
+	w.log.Info(fmt.Sprintf(format, v...))
+}
