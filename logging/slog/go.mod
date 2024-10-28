3e3106028b4b1e7c24621ace46fe4960b334e5d1
zstone12 <522089185@qq.com>
2024-10-28 18:32:37 +0800

    [wip]feat: add unit test

--- logging/slog/go.mod
+++ logging/slog/go.mod
@@ -3,6 +3,7 @@ module gorm.io/plugin/opentelemetry/logging/slog
 go 1.22
 
 require (
+	github.com/stretchr/testify v1.9.0
 	go.opentelemetry.io/otel v1.31.0
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.31.0
 	go.opentelemetry.io/otel/sdk v1.31.0
@@ -12,13 +13,16 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.opentelemetry.io/otel/metric v1.31.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- logging/slog/logger_test.go
+++ logging/slog/logger_test.go
@@ -1,7 +1,12 @@
 package slog
 
 import (
+	"bytes"
 	"context"
+	"github.com/stretchr/testify/assert"
+	"log/slog"
+	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -73,4 +78,94 @@ func TestLogger(t *testing.T) {
 	db.Logger.Info(ctx, "no trace context")
 
 	errSpan.End()
+}
+
+func TestLogger1(t *testing.T) {
+	ctx := context.Background()
+
+	buf := new(bytes.Buffer)
+
+	shutdown := stdoutProvider(ctx)
+	defer shutdown()
+
+	logger := logger.New(
+		NewWriter(
+			WithTraceErrorSpanLevel(slog.LevelWarn),
+			WithRecordStackTraceInSpan(true),
+		), logger.Config{
+			SlowThreshold: time.Millisecond,
+			LogLevel:      logger.Warn,
+			Colorful:      false,
+		})
+
+	logger.Info(ctx, "log from origin slog")
+	assert.True(t, strings.Contains(buf.String(), "log from origin slog"))
+	buf.Reset()
+
+	tracer := otel.Tracer("test otel std logger")
+
+	ctx, span := tracer.Start(ctx, "root")
+
+	assert.True(t, strings.Contains(buf.String(), "trace_id"))
+	assert.True(t, strings.Contains(buf.String(), "span_id"))
+	assert.True(t, strings.Contains(buf.String(), "trace_flags"))
+	buf.Reset()
+
+	span.End()
+
+	ctx, child := tracer.Start(ctx, "child")
+
+	child.End()
+
+	_, errSpan := tracer.Start(ctx, "error")
+
+	errSpan.End()
+}
+
+func TestLogLevel(t *testing.T) {
+	buf := new(bytes.Buffer)
+
+	logger := NewWriter(
+		WithTraceErrorSpanLevel(slog.LevelWarn),
+		WithRecordStackTraceInSpan(true),
+	)
+
+	/*	// output to buffer
+		logger.SetOutput(buf)
+
+		logger.Debug("this is a debug log")
+		assert.False(t, strings.Contains(buf.String(), "this is a debug log"))
+
+		logger.SetLevel(klog.LevelDebug)
+	*/
+	logger.log.Debug("this is a debug log msg")
+	assert.True(t, strings.Contains(buf.String(), "this is a debug log"))
+}
+
+func TestLogOption(t *testing.T) {
+	buf := new(bytes.Buffer)
+
+	lvl := new(slog.LevelVar)
+	lvl.Set(slog.LevelDebug)
+	logger := NewWriter(
+		WithLevel(lvl),
+		WithOutput(buf),
+		WithHandlerOptions(&slog.HandlerOptions{
+			AddSource: true,
+			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+				if a.Key == slog.MessageKey {
+					msg := a.Value.Any().(string)
+					msg = strings.ReplaceAll(msg, "log", "new log")
+					a.Value = slog.StringValue(msg)
+				}
+				return a
+			},
+		}),
+	)
+
+	logger.log.Debug("this is a debug log")
+	assert.True(t, strings.Contains(buf.String(), "this is a debug new log"))
+
+	dir, _ := os.Getwd()
+	assert.True(t, strings.Contains(buf.String(), dir))
 }
